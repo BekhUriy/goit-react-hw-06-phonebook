ef2829f8f5e5f8168bbf6df2a92897d83b95df14
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-01-31 12:45:04 +0200

    Update contactsSlice.js

--- src/components/contactsSlice.js
+++ src/components/contactsSlice.js
@@ -1,19 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const loadContactsFromLocalStorage = () => {
+  const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+  return storedContacts ? storedContacts : [];
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    items: [],
+    items: loadContactsFromLocalStorage(),
     filter: '',
   },
   reducers: {
     addContact: (state, action) => {
       state.items.push(action.payload);
+      localStorage.setItem('contacts', JSON.stringify(state.items));
     },
     deleteContact: (state, action) => {
       state.items = state.items.filter(
         contact => contact.id !== action.payload
       );
+      localStorage.setItem('contacts', JSON.stringify(state.items));
     },
     setFilter: (state, action) => {
       state.filter = action.payload;
